83b249fb48472ca50072f977792ddbd0e1506d7b
Sven-Erik Jonsson <sven.erik@geta.no>
2020-08-25 16:16:58 +0200

    First session

---
+++ Session01/Session01-Example/Enum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_Example
+{
+    enum MyEnum : int
+    {
+        Unknown = 0,
+        Http = 1
+    }
+}
---
+++ Session01/Session01-Example/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+
+            /* This is a multiline
+             * comment */
+
+            int[] myArray = new int[] { 1, 2, 4, 5 };
+
+            int myArrayInteger = myArray.Length;
+
+            int? integer = null;
+
+            if (true)
+            {
+
+                // Strängkonkatenering, sammanslagning
+                string data = "this integer has value" + integer.ToString();
+
+                // Stränginterpolation
+                string secondData = $"this integer has value {integer}";
+
+                // Strängformattering
+                string thirdData = string.Format("this integer has value {0}", integer);
+
+                // Implicit strängkonkatenering
+                string fourthData = "this integer has value" + integer; // Gör ej
+
+                Console.WriteLine(fourthData);
+
+            }
+
+            #region console interaction
+
+
+
+            Console.ReadLine();
+
+            Print("value to print");
+
+            #endregion
+
+            #if Debug
+                
+            #endif
+        }
+
+        static string Print(object value)
+        {
+            return string.Empty;
+        }
+
+        static int Print(int value)
+        {
+            return 0;
+        }
+
+        static string Print(object valueOne, object valueTwo)
+        {
+            var stringValue = valueOne.ToString();
+            Console.WriteLine("Värdet är: " + stringValue);
+            
+            return stringValue;
+        }
+    }
+}
