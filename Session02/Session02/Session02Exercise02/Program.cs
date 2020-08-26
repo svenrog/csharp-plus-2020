19f6b5b476d133742caddf980489341f25c23295
Sven-Erik Jonsson <sven.erik@geta.no>
2020-08-26 12:00:01 +0200

    Session02Excercise02

---
+++ Session02/Session02/Session02Exercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Session02Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to enter your name? (y/n)?");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'n')
+                return;
+
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
