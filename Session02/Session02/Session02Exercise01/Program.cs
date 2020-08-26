2177031157cda8cc592c01d82666a3813b51bcc4
Sven-Erik Jonsson <sven.erik@geta.no>
2020-08-26 10:38:35 +0200

    String test

--- Session02/Session02/Session02Exercise01/Program.cs
+++ Session02/Session02/Session02Exercise01/Program.cs
@@ -7,6 +7,10 @@ namespace Session02Exercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string stringValue = "MyStringValue";
+
+            Console.WriteLine("The value of stringValue is: " + stringValue);
         }
     }
 }
