4b353b47ba674224eb0695d861101a9033397d3e
Sven-Erik Jonsson <sven.erik@geta.no>
2020-08-28 09:16:00 +0200

    Session 03 - Excercises 01 and 02

---
+++ Session03/Session03/Session03Excercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Session03Excercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integerValues = new [] { 1, 2, 3 };
+            var integerValuesName = nameof(integerValues);
+
+            for (var i = 0; i < integerValues.Length; i++)
+            {
+                var value = integerValues[i];
+
+                Console.WriteLine($"Index {i} i arrayen {integerValuesName} har värdet: {value}");
+            }
+
+            var doWhileIndex = 0;
+
+            do
+            {
+                var value = integerValues[doWhileIndex];
+
+                Console.WriteLine($"Do while Index {doWhileIndex} i arrayen {integerValuesName} har värdet: {value}");
+
+                doWhileIndex++;
+            }
+            while (doWhileIndex < integerValues.Length);
+
+            var whileIndex = 0;
+
+            while (whileIndex < integerValues.Length)
+            {
+                var value = integerValues[whileIndex];
+
+                Console.WriteLine($"While Index {whileIndex} i arrayen {integerValuesName} har värdet: {value}");
+
+                whileIndex++;
+            }
+        }
+    }
+}
---
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Session03Excercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputArray)
+            {
+                Console.WriteLine("Värdet är " + number);
+            }
+
+        }
+    }
+}
